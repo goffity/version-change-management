33b46281b30518bd7eb93d8d83b1d826a20340d0
Goffity <goffity@gmail.com>
2022-12-13 02:37:06 +0700

    BREAKING CHANGE: use google cloud function to handle request.

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const express = require("express");
-const app = express();
+const functions = require("@google-cloud/functions-framework");
 
-app.get("/temp", function (req, res) {
-  res.status(200).send("OK");
-});
-app.get("/", (req, res) => {
+functions.http("hello", (req, res) => {
   res.status(200).send({
     message: "OK",
   });
-});
-
-app.listen(3000, () => {
-  console.log(`APp listening on port 3000`);
 });
