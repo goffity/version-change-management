cd8b39fd1664d8a2399aefb1ab1b5477dec58759
Goffity <goffity@gmail.com>
2022-12-13 02:47:25 +0700

    feat!: use google cloud function to handle request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const functions = require("@google-cloud/functions-framework");
 
-functions.http("hello_function", (req, res) => {
+functions.http("first_function", (req, res) => {
   res.status(200).send({
     message: "OK",
   });
