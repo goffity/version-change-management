5e70d33e16ea8e373aea84c3d16698eeaf0ab4f8
Goffity <goffity@gmail.com>
2022-12-13 01:50:57 +0700

    BREAKING CHANGE: Added express to handle request.

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/temp", function (req, res) {
+    res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log(`APp listening on port 3000`);
+});
